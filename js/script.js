61f1d0f5901378cf857953de35cbdbc1d04d2ad5
Isha Joglekar <ijoglekar16@gmail.com>
2023-12-08 00:54:35 +0000

    ResCounty Version 1.0

---
+++ js/script.js
@@ -0,0 +1,230 @@
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "https://code.jquery.com/jquery-2.2.1.min.js";
+import ResilientSDK from 'https://cdn.resilientdb.com/resilient-sdk.js';
+
+const sdk = new ResilientSDK();
+
+// Then bind the event to the callback function.
+// There are several events for cross browser compatibility.
+script.onreadystatechange = handler;
+script.onload = handler;
+
+// Fire the loading
+head.appendChild(script);
+
+var flag;
+var currentRole;
+
+var user_name;
+var ssn;
+var role;
+
+var user_public_key;
+
+document.getElementById('formreg')!= null ? document.getElementById('formreg').addEventListener('submit', handleRegistration) : '';
+document.getElementById('formlogin')!= null ? document.getElementById('formlogin').addEventListener('submit', handleLogin) : '';
+
+document.getElementById('formaddland')!= null ? document.getElementById('formaddland').addEventListener('submit', handleAddLand) : '';
+
+function handler(){
+    console.log('jquery added :)');
+}
+
+var overlay; // Declare a variable to store the overlay element
+
+function showLoader() {
+  // Create overlay element
+  overlay = document.createElement("div");
+  overlay.className = "overlay";
+  document.body.appendChild(overlay);
+
+  // Create loader element
+  var loader = document.createElement("div");
+  loader.className = "loader";
+  overlay.appendChild(loader);
+}
+
+// Function to hide the loader
+function hideLoader() {
+  var loaderContainer = document.querySelector(".loader-container");
+  if (loaderContainer) {
+    loaderContainer.remove();
+  }
+  if (overlay) {
+    overlay.remove(); 
+    overlay = null;   
+  }
+}
+
+
+sdk.addMessageListener((event) => {
+    const message = event.data.data;
+    var nexres_response = message;
+    console.log(nexres_response);
+    console.log(flag);
+
+    if (flag == "create-user") {
+      console.log("inside addMessageListener");
+      var user_public_key = nexres_response;
+      localStorage.setItem("currentPublicKey", user_public_key);
+      var base_initial_amount = 10000;
+      var user_details = ` "name": "${user_name}", "ssn": "${ssn}",  "role": "${role}",  "type": "balance", "timestamp": ${new Date().getTime()}`
+      // commit - i.e. send user details to resDb
+      flag = "show-dashboard";
+      sdk.sendMessage({
+        direction: "commit-page-script",
+        message: user_details,
+        amount: base_initial_amount,
+        address: user_public_key,
+      });
+    } else if (flag == "show-dashboard") {
+      console.log("user registered");
+      showLoader();
+      setTimeout(function () {
+      if (currentRole == "buyer") {
+        window.location.href = './buyer_dashboard.html'
+      } else if (currentRole == "seller") {
+        window.location.href = './seller_dashboard.html'
+      }
+      hideLoader();
+    }, 3000);
+
+      flag = "user-logged-in";
+    }else if(flag == "land-added"){
+      alert("Land Added")
+      window.location.href = './seller_dashboard.html'
+      
+    } else if (flag=="user-logged-in") {
+      console.log("bugggggggggg");
+      alert("bugggggggggg");
+    }
+
+});
+
+function handleAddLand(event) {
+  event.preventDefault();
+  $("#initial").fadeOut("normal", function(){
+    $("#loader").fadeIn("normal");
+  });
+  flag = "create-user";
+  
+
+  const formData = new FormData(event.target);
+  var land_name = formData.get("city");
+  var state = formData.get("state");
+  var price = formData.get("price");
+  var plot_no = formData.get("plot_no");
+  var zip_code = formData.get("zip_code");
+  var type = "land";
+  console.log(price);
+
+  var land_details = `"id":"${plot_no+zip_code}" , "name": "${land_name}", "state": "${state}",  "price": "${price}" , "type":"${type}",  "timestamp": ${new Date().getTime()}, "status": "available"`
+  user_public_key = localStorage.getItem('currentPublicKey')
+  sdk.sendMessage({
+    direction: "commit-page-script",
+    message: land_details,
+    address: user_public_key,
+    amount: price
+  });
+   flag = "land-added"
+}
+
+function handleRegistration(event) {
+  event.preventDefault();
+  $("#initial").fadeOut("normal", function(){
+    $("#loader").fadeIn("normal");
+  });
+  flag = "create-user";
+  sdk.sendMessage({
+    direction: "account-page-script",
+  });
+
+  const formData = new FormData(event.target);
+  user_name = formData.get("name");
+  ssn = formData.get("ssn");
+  role = formData.get("role");
+  
+  if(formData.get('role') == 'role-buyer'){
+    currentRole = "buyer";
+  }else if(formData.get('role') == 'role-seller'){
+    currentRole = "seller";
+  }
+  localStorage.setItem('role', currentRole);
+  localStorage.setItem('user_name', user_name);
+}
+
+function handleLogin(event) {
+  event.preventDefault();
+  $("#initial").fadeOut("normal", function(){
+    $("#loader").fadeIn("normal");
+  });
+  flag = "create-user";
+  sdk.sendMessage({
+    direction: "account-page-script",
+  });
+
+  const formData = new FormData(event.target);
+  user_name = formData.get("name");
+  ssn = formData.get("ssn");
+  role = formData.get("role");
+  
+  if(formData.get('role') == 'role-buyer'){
+    currentRole = "buyer";
+  }else if(formData.get('role') == 'role-seller'){
+    currentRole = "seller";
+  }
+  localStorage.setItem('role', currentRole);
+  localStorage.setItem('user_name', user_name);
+}
+
+// export async function getAllLands() {
+//   var transactions = await getAllTransactions(user_public_key);
+//   console.log(transactions);
+// }
+
+// function getAllTransactions(key) {
+//   const url = 'https://cloud.resilientdb.com/graphql';
+//   const graphqlQuery = `
+//       query {
+//           getFilteredTransactions(filter: {
+//               ownerPublicKey: ""
+//               recipientPublicKey: "${key}"
+//           }) {
+//               id
+//               version
+//               amount
+//               metadata
+//               operation
+//               asset
+//               publicKey
+//               uri
+//               type
+//           }
+//       }
+//   `;
+
+//   const payload = {
+//       query: graphqlQuery
+//   };
+
+//   const headers = {
+//       'Content-Type': 'application/json',
+//       // Add any other headers as needed (e.g., authorization headers)
+//   };
+
+//   return fetch(url, {
+//       method: 'POST',
+//       headers: headers,
+//       body: JSON.stringify(payload)
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//       return data;
+//   })
+//   .catch(error => {
+//       console.error('Error:', error);
+//       setTimeout(() => longPoll(key), 15000); // Retry after 5 seconds in case of error
+//   });
+// }
---
+++ js/seller.js
@@ -0,0 +1,320 @@
+
+import ResilientSDK from 'https://cdn.resilientdb.com/resilient-sdk.js';
+
+const sdk = new ResilientSDK();
+
+var flag;
+
+export function getAllTransactions(key='') {
+    const url = 'https://cloud.resilientdb.com/graphql';
+    const graphqlQuery = `
+        query {
+            getFilteredTransactions(filter: {
+                ownerPublicKey: ""
+                recipientPublicKey: "${key}"
+            }) {
+                id
+                version
+                amount
+                metadata
+                operation
+                asset
+                publicKey
+                uri
+                type
+            }
+        }
+    `;
+  
+    const payload = {
+        query: graphqlQuery
+    };
+  
+    const headers = {
+        'Content-Type': 'application/json',
+        // Add any other headers as needed (e.g., authorization headers)
+    };
+  
+    return fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(payload)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const getTimestamp = (obj) => {
+
+            const new_obj = JSON.parse(obj.asset.replace(/'/g, '"'));
+            return new_obj.data.timestamp || undefined;
+        };
+        // console.log(data);
+        const sortedData = data.data.getFilteredTransactions
+                                .filter(item => getTimestamp(item) !== undefined) // Filter out items without a timestamp
+                                .sort((a, b) => getTimestamp(b) - getTimestamp(a));
+
+        return sortedData;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        setTimeout(() => longPoll(key), 15000); // Retry after 5 seconds in case of error
+    });
+}
+
+export async function getAllLands(user_public_key) {
+    return getAllTransactions(user_public_key)
+        .then(data => {
+            var lands = [];
+            for (let i=0; i<data.data.getFilteredTransactions.length; i++) {
+                str_data = data.data.getFilteredTransactions[i].asset;
+                var correctedJsonString = str_data.replace(/'/g, '"');
+                try {
+                    var jsonObject = JSON.parse(correctedJsonString);
+                    if (jsonObject.data.type == "land") {
+                        lands.push(jsonObject.data);
+                    }
+                } catch (e) {
+                    console.error("Error parsing JSON string:", e);
+                }
+            }
+            return lands
+        })
+        .catch(error => {
+            console.log("Error = ", error);
+        });
+}
+
+export async function getSellerData(user_public_key) {
+    var transactions = await getAllTransactions(user_public_key);
+    var lands = [];
+    var currentBalance;
+
+    for (let i=0; i<transactions.length; i++) {
+        var str_data = transactions[i].asset;
+        var correctedJsonString = str_data.replace(/'/g, '"');
+        try {
+            var jsonObject = JSON.parse(correctedJsonString);
+            if (jsonObject.data.type == "balance") {
+                currentBalance = transactions[i].amount;
+                break;
+            }
+        } catch (e) {
+            console.error("Error parsing JSON string:", e);
+        }
+    }
+    let landStatuses = {};
+    for (let i=0; i<transactions.length; i++) {
+        var str_data = transactions[i].asset;
+        var correctedJsonString = str_data.replace(/'/g, '"');
+        try {
+            var jsonObject = JSON.parse(correctedJsonString);
+            if (jsonObject.data.type == "land") {
+                if (landStatuses[jsonObject.data.id] && landStatuses[jsonObject.data.id]["status"] == "sold") {
+                    continue;
+                }
+                landStatuses[jsonObject.data.id] = {
+                    "status": jsonObject.data.status,
+                }
+            }
+        } catch (e) {
+            console.error("Error parsing JSON string:", e);
+        }
+    }
+    for (let i=0; i<transactions.length; i++) {
+        var str_data = transactions[i].asset;
+        var correctedJsonString = str_data.replace(/'/g, '"');
+        try {
+            var jsonObject = JSON.parse(correctedJsonString);
+            if (jsonObject.data.type == "land") {
+                if (landStatuses[jsonObject.data.id]["status"] == "sold" && jsonObject.data.status=="available") {
+                    continue;
+                }
+                lands.push(jsonObject.data);
+            }
+        } catch (e) {
+            console.error("Error parsing JSON string:", e);
+        }
+    }
+    var payload = {
+        "lands": lands,
+        "currentBalance": currentBalance
+    }
+    return payload;
+}
+  
+export async function getBuyerData() {
+    var transactions = await getAllTransactions();
+    var lands = [];
+    var currentBalance;
+    var buyerId;
+    var sellerPublicKey;
+
+    // loop over all transactions to collect all the sellers and their ids and balance in a dictionary
+    let sellers = {};
+    for (let i=0; i<transactions.length; i++) {
+        var str_data = transactions[i].asset;
+        var correctedJsonString = str_data.replace(/'/g, '"');
+        try {
+            var jsonObject = JSON.parse(correctedJsonString);
+            if (jsonObject.data.role == "role-seller") {
+                var sellerData = await getSellerData(transactions[i].publicKey)
+                sellers[transactions[i].publicKey] = {
+                    "sellerId": transactions[i].id,
+                    "currentBalance": sellerData.currentBalance
+                }
+            }
+        } catch (e) {
+            console.error("Error parsing JSON string:", e);
+        }
+    }
+
+    let landStatuses = {};
+    for (let i=0; i<transactions.length; i++) {
+        var str_data = transactions[i].asset;
+        var correctedJsonString = str_data.replace(/'/g, '"');
+        try {
+            var jsonObject = JSON.parse(correctedJsonString);
+            if (jsonObject.data.type == "land") {
+                if (landStatuses[jsonObject.data.id] && landStatuses[jsonObject.data.id]["status"] == "sold") {
+                    continue;
+                }
+                landStatuses[jsonObject.data.id] = {
+                    "status": jsonObject.data.status,
+                }
+            }
+        } catch (e) {
+            console.error("Error parsing JSON string:", e);
+        }
+    }
+    console.log("landStatuses = ", landStatuses);
+
+    for (let i=0; i<transactions.length; i++) {
+        var str_data = transactions[i].asset;
+        var correctedJsonString = str_data.replace(/'/g, '"');
+        try {
+            var jsonObject = JSON.parse(correctedJsonString);
+            if (jsonObject.data.type == "balance" 
+                    && transactions[i].publicKey == localStorage.getItem("currentPublicKey")) {
+                currentBalance = transactions[i].amount;
+                buyerId = transactions[i].id;
+                break;
+            }
+        } catch (e) {
+            console.error("Error parsing JSON string:", e);
+        }
+    }
+
+    for (let i=0; i<transactions.length; i++) {
+        var str_data = transactions[i].asset;
+        var correctedJsonString = str_data.replace(/'/g, '"');
+        try {
+            var jsonObject = JSON.parse(correctedJsonString);
+            if (jsonObject.data.type == "land") {
+                sellerPublicKey = transactions[i].publicKey;
+                jsonObject.data["sellerPublicKey"] = sellerPublicKey;
+                jsonObject.data["sellerId"] = sellers[sellerPublicKey]["sellerId"];
+                jsonObject.data["sellerCurrentBalance"] = sellers[sellerPublicKey]["currentBalance"];
+                jsonObject.data["landId"] = transactions[i].id;
+                if (landStatuses[jsonObject.data.id]["status"] == "sold" && jsonObject.data.status=="available") {
+                    continue;
+                }
+                lands.push(jsonObject.data);
+            }
+        } catch (e) {
+            console.error("Error parsing JSON string:", e);
+        }
+    }
+    var payload = {
+        "lands": lands,
+        "currentBalance": currentBalance,
+        "buyerId": buyerId
+    }
+    return payload;
+}
+
+export async function buyNow(id, name, state, price, buyerCurrentBalance, buyerId, sellerCurrentBalance, sellerPublicKey, sellerId, landId){
+price = parseInt(price);
+sellerCurrentBalance = parseInt(sellerCurrentBalance);
+buyerCurrentBalance = parseInt(buyerCurrentBalance)
+if (price > buyerCurrentBalance) {
+    alert("insufficient Balance");
+} else {
+    console.log("Sufficient Balance");
+    var buyerUpdatedBalance = -price + buyerCurrentBalance;
+    var sellerUpdatedBalance = price + sellerCurrentBalance;
+    var withdraw_message = `"timestamp": ${new Date().getTime()}, "type": "balance"`;
+
+    // update land to sold - payload
+    var land_details = `"id":${id}, "name": "${name}", "state": "${state}",  "price": ${price} , "type":"land",  "timestamp": ${new Date().getTime()}, "status": "sold"`
+
+    const valuesList = [
+        {
+            id: buyerId,
+            message: withdraw_message,
+            amount: buyerUpdatedBalance,
+            address: localStorage.getItem("currentPublicKey"),
+        },
+        {
+            id: sellerId,
+            message: withdraw_message,
+            amount: sellerUpdatedBalance,
+            address: sellerPublicKey,
+        },
+        {
+            id: landId,
+            message: land_details,
+            amount: price,
+            address: sellerPublicKey
+        }
+    ];
+
+    sdk.sendMessage({
+        direction: "update-multi-page-script",
+        values: valuesList,
+    });
+    flag = "update-land-status-to-sold";
+}
+}
+
+sdk.addMessageListener((event) => {
+    const message = event.data.data;
+    var nexres_response = message;
+    console.log(nexres_response);
+    console.log(flag);
+
+    if (flag == "submit-payment") {
+      var user_public_key = nexres_response;
+      localStorage.setItem("currentPublicKey", user_public_key);
+      var base_initial_amount = 10000;
+      var user_details = ` "name": "${user_name}", "ssn": "${ssn}",  "role": "${role}",  "type": "balance", "timestamp": ${new Date().getTime()}`
+
+      flag = "show-dashboard";
+      sdk.sendMessage({
+        direction: "commit-page-script",
+        message: user_details,
+        amount: base_initial_amount,
+        address: user_public_key,
+      });
+
+    } else if (flag == "show-dashboard") {
+      console.log("user registered");
+      showLoader();
+      setTimeout(function () {
+      if (currentRole == "buyer") {
+        window.location.href = './buyer_dashboard.html'
+      } else if (currentRole == "seller") {
+        window.location.href = './seller_dashboard.html'
+      }
+      hideLoader();
+    }, 3000);
+
+      flag = "user-logged-in";
+    }else if(flag == "land-added"){
+      alert("Land Added")
+      window.location.href   = './seller_dashboard.html'
+      
+    } else if (flag=="user-logged-in") {
+    } else if (flag == "update-land-status-to-sold") {
+        // commit message to update land
+        alert("Land Sold");
+    }
+});
